dddeb514de69429c70b10fc82955fabd644f357b
SanjoliGoyal1262 <sanjoli1262.be21@chitkara.edu.in>
2025-10-21 18:39:33 +0530

    Add[Sanjoli] : Adding delete functionality of Admin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -88,3 +88,31 @@ export const updateUserDetails = async (req, res) => {
     });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (req.user._id.toString() === id) {
+      return res.status(400).json({ message: "Admins cannot delete themselves" });
+    }
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.role === 2) {
+      return res.status(403).json({ message: "Cannot delete another admin" });
+    }
+
+    await user.deleteOne();
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error("Delete User Error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
--- routes/admin/adminRoutes.js
+++ routes/admin/adminRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { adminOnly, protect } from "../../middlewares/authMiddleware.js";
-import { getAllUsers } from "../../controllers/adminController.js";
+import { deleteUser, getAllUsers, updateUserDetails } from "../../controllers/adminController.js";
 
 const router = express.Router();
 
 router.get("/users", protect, adminOnly, getAllUsers);
+router.put("/users/:id", protect, adminOnly, updateUserDetails);
+router.delete("/users/:id", protect, adminOnly, deleteUser);
 
 export default router;
